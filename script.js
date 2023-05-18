205bc20b7c0dcc35c6f40bc2f0e62f9452de4dae
AndreiZhura <zhura2296@yandex.ru>
2023-05-18 23:45:41 +0300

    1

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const buttonTestClose = document.querySelector('.popup-test_cross');
 const passTheTest = document.querySelector('.choice-condicioner__pass-test');
 const popupTest = document.querySelector('.popup-test');
 
+const slider = document.querySelector('.popup-test__questions');
+const sliderChildren = Array.from(slider.children);
+const questionItem = document.querySelector('.popup-test__question-item');
+
+const buttonEnd = document.querySelector('.popup-test__button_end');
+const buttonUp = document.querySelector('.popup-test__button_up');
+
+let counter = 1;
 
 function openBurgerMenu() {
     menuBurger.classList.remove('menu-burger_none');
@@ -23,9 +31,35 @@ const closeTest = () => {
     popupTest.classList.add('popup-test_none');
 }
 
+//Test
+
+sliderChildren.forEach(function (slide, index) {
+    slide.classList.add('popup-test__question-item_none');
 
 
 
+    if (index === 0) {
+        slide.classList.remove('popup-test__question-item_none');
+    }
+
+    slide.dataset.index = index;
+})
+
+
+buttonUp.addEventListener('click', function (event) {
+    event.preventDefault();
+   const one =  slider.querySelector('.popup-test__question-item_none')
+   const curindex = one.dataset.index;
+   console.log(curindex)
+   let step = sliderChildren[counter++]
+  
+   if (step === curindex) {
+       step.classList.remove('popup-test__question-item_none');
+   }
+ 
+   
+    
+})
 
 burgerButton.addEventListener('click', openBurgerMenu);
 menuBurgerClose.addEventListener('click', closeBurgerMenu);
@@ -34,3 +68,5 @@ menuBurgerItem.addEventListener('click', event => {
 });
 passTheTest.addEventListener('click', openTest);
 buttonTestClose.addEventListener('click', closeTest);
+
+
