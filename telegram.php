063476324b7633b4b74139669d2f0989464db2bf
AndreiZhura <zhura2296@yandex.ru>
2023-05-20 13:58:29 +0300

    делаю тест

--- telegram.php
+++ telegram.php
@@ -1,11 +1,15 @@
 <?php
 $name = $_POST['name'];
 $phone = $_POST['phone'];
+$answerOne = $_POST['answer_1'];
+$answerTwo = $_POST['answer_2'];
 $token = "5618935755:AAFX4G8Ou3xCBoQqcd98ezPzs06iSdBD1Fo";
 $chat_id = "-954869352";
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телефон: ' => $phone,
+  'Вопрос-1' => $answerOne, 
+  'Вопрос-2' => $answerTwo 
 );
 
 foreach($arr as $key => $value) {
